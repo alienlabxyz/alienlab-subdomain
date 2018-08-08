4e17e118d9a6c39e11b57b50836ad2c723c10ea9
Levon <levonsargsyan1990@gmail.com>
2018-08-08 20:59:15 +0400

    window existence checked

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,28 @@
 import React from 'react'
+import { withPrefix } from 'gatsby-link'
 
-const IndexPage = () => {
-  const url = window.location.host;
-  const parsedUrl = url.split('.');
-  const [ subdomain, domain, type ] = parsedUrl;
-  const checkLength = url.includes('localhos') ? 2 : 3;
-  
-  if (parsedUrl.length < checkLength) {
+const IndexPage = (props) => {
+  console.log(withPrefix('/'))
+  if (typeof window !== 'undefined') { 
+    const url = window.location.host;
+    const parsedUrl = url.split('.');
+    const [ subdomain, domain, type ] = parsedUrl;
+    const checkLength = url.includes('localhost') ? 2 : 3;
+    
+    if (parsedUrl.length < checkLength) {
+      return (
+        <div style={{ textAlign: 'center', paddingTop: '200px' }}>
+        <h1 style={{ fontWeight: 100 }}>Looks like you are not looking for a specific team, do you?<br /><br />o_O</h1>
+        </div>
+      )
+    }
     return (
       <div style={{ textAlign: 'center', paddingTop: '200px' }}>
-        <h1 style={{ fontWeight: 100 }}>Looks like you are not looking for a specific team, do you?<br /><br />o_O</h1>
+        <h1 style={{ fontWeight: 100 }}>Hello and welcome to <span style={{ fontWeight: 800 }}>{subdomain}</span> team {domain === 'dev' ? 'on Development server' : ''}<br /><br /> ^_^</h1>
       </div>
     )
   }
-  return (
-    <div style={{ textAlign: 'center', paddingTop: '200px' }}>
-      <h1 style={{ fontWeight: 100 }}>Hello and welcome to <span style={{ fontWeight: 800 }}>{subdomain}</span> team {domain === 'dev' ? 'on Development server' : ''}<br /><br /> ^_^</h1>
-    </div>
-  )
+  return null;
 }
 
 export default IndexPage
